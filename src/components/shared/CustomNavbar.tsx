12d14ba453012df72cb6ebcbef3c75d3eda5c44e
MIRNOMAN <abdullahalnoman1509@gmail.com>
2025-02-03 01:52:32 +0600

    create navber link

---
+++ src/components/shared/CustomNavbar.tsx
@@ -0,0 +1,24 @@
+
+
+export const links = [
+    {
+      title: "About us",
+      href: "/about-us",
+      secure: false,
+    },
+    {
+      title: "Contact us",
+      href: "/contact-us",
+      secure: false,
+    },
+    {
+      title: "Profile",
+      href: "/profile",
+      secure: true,
+    },
+    {
+      title: "Dashboard",
+      href: "/admin/users",
+      secure: true,
+    },
+  ];
